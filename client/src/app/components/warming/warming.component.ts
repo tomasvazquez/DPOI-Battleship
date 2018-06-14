94de280fffa2b1bf97263032de503d03c734d43c
Nacho <ignacio.vazquez@ing.austral.edu.ar>
2018-06-14 19:44:33 -0300

    Changed listened port.

--- client/src/app/components/warming/warming.component.ts
+++ client/src/app/components/warming/warming.component.ts
@@ -51,7 +51,7 @@ export class WarmingComponent implements OnInit {
     window.localStorage.setItem("futureState",undefined);
     this.initWarming();
     var options = {"transports":["polling"]};
-    this.socket = io('http://localhost:3000',options);
+    this.socket = io('https://stark-oasis-79231.herokuapp.com',options);
     this.socket.emit('message', this.user);
     this.socket.emit('getStatus', this.user);
     this.userData.setSocket(this.socket);
